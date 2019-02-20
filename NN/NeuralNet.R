125d7337a5ed349ff30c8c8544e36e09d6ceadf2
eugene9212 <eugene12@korea.ac.kr>
2019-02-20 10:07:23 +0900

    Test for Adam update

--- NN/NeuralNet.R
+++ NN/NeuralNet.R
@@ -1,6 +1,8 @@
 ##################################
 ## NN for binary classification ##
 ##################################
+# This is for practice (Guideline of how to use uploaded Neural Net code)
+# Load the functions related with Neaural Net
 rm(list=ls())
 setwd("C:/Users/eugene/Desktop/Machine_Learning_with_R/NN")
 source('fn/train.NN.R')    # Train Neural Network
@@ -11,13 +13,13 @@ source('fn/sigmoid.R')     # Calculate Sigmoid (used in train function)
 source('fn/predict.NN.R')  # predict Neural Network
 source('fn/ROC.R')         # Roc function and confusion matrix
 
-##Load the data
-train=read.table("data/trn.txt")
-test=read.table("data/tst.txt")
+# Load the data
+train <- read.table("data/trn.txt")
+test <- read.table("data/tst.txt")
 
 # split input and output
-in.trn = as.matrix(train[,1:13])
-out.trn = as.matrix(train[,14])
+in.trn <- as.matrix(train[,1:13])
+out.trn <- as.matrix(train[,14])
 
 # shape of train data
 trn.shape <- dim(in.trn) # 60290개 데이터, 13차원(60290 X 13)
@@ -25,53 +27,58 @@ trn.shape <- dim(in.trn) # 60290개 데이터, 13차원(60290 X 13)
 # TRAIN
 obj.train.nn <- train.NN(train.x = in.trn, train.y = out.trn, 
                          w.type = "RBM", s.d = 0, layer.units = c(3,4,3), 
-                         update = "momentum", learning = 0.001, hyper.p = 0.007, epoch = 5)
+                         update = "Adam", learning = 0.001, hyper.p = c(0.9,0.999), epoch = 1)
 
 # TEST
 in.tst <- as.matrix(test[,1:13])
 out.tst <- as.matrix(test[,14])
 obj.test.nn <- predict.NN(test.x = in.tst, test.y = out.tst, obj = obj.train.nn)
 
-table(obj.test.nn$output)
+# Make a histogram for predicted value
+# table(obj.test.nn$output)
 hist(obj.test.nn$output*10^6)
 
-predict_y = matrix(1, nrow = dim(obj.test.nn$output)[1],ncol = dim(obj.test.nn$output)[2])
+predict_y  <-  matrix(1, nrow = dim(obj.test.nn$output)[1],ncol = dim(obj.test.nn$output)[2])
 
 ######################
 ## create ROC curve ##
 ######################
-
-rslt <- roc_function(x = 0.516052165, predicted.y = obj.test.nn$output, true.y = out.tst)
+# find out the optimal cutting point
+rslt <- roc_function(x = 0.507, predicted.y = obj.test.nn$output, true.y = out.tst)
 rslt$error
 rslt$ROC
 rslt$confusion
 rslt$error
-dots = rbind(roc_function(0, predicted.y = obj.test.nn$output, true.y = out.tst)$ROC, 
+dots <- rbind(roc_function(0, predicted.y = obj.test.nn$output, true.y = out.tst)$ROC, 
              roc_function(0.51605214, predicted.y = obj.test.nn$output, true.y = out.tst)$ROC,
              roc_function(0.516052165, predicted.y = obj.test.nn$output, true.y = out.tst)$ROC,
              roc_function(0.51605216511, predicted.y = obj.test.nn$output, true.y = out.tst)$ROC,
-             roc_function(7, predicted.y = obj.test.nn$output, true.y = out.tst)$ROC,
+             roc_function(0.7, predicted.y = obj.test.nn$output, true.y = out.tst)$ROC,
              roc_function(1, predicted.y = obj.test.nn$output, true.y = out.tst)$ROC)
-colnames(dots)<-c("FPR","TPR")
-dots<-data.frame(dots)
+colnames(dots) <- c("FPR","TPR")
+dots <- data.frame(dots)
+
+# Draw ROC curve
 plot(dots,type="o",col="blue")
-line=lines(x = c(0,1), y = c(1,0), col = 2, lty = 2, lwd = 2)
+line <- lines(x = c(0,1), y = c(1,0), col = 2, lty = 2, lwd = 2)
 
 
 for(i in 1:dim(output)[1])
 {
   if (output[i]>=0.50543){
-    predict_y[i]="1"
+    predict_y[i] <- "1"
   }
-  else predict_y[i]="0"
+  else predict_y[i] <- "0"
 }
 
-
-error = (1-mean(predict_y==out.tst))*100
+# Calculate the error rate
+error <- (1-mean(predict_y==out.tst))*100
 error
 
 
-#Confusion matrix
+# Confusion matrix
 rslt$confusion
 
-  
+## Conclusion ##
+# With this test set Adam update showed the best performance
+
